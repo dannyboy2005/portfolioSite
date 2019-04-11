967d912e3fe6d0bbda43e0b6262dd58cf0c6f240
Daniel Boffey <dboffey70@googlemail.com>
2019-04-11 22:21:42 +0100

    Social media changes, added animate css

--- docs/js/app.js
+++ docs/js/app.js
@@ -157,3 +157,18 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+// Animate CSS function
+function animateCSS(element, animationName, callback) {
+  const node = document.querySelector(element)
+  node.classList.add('animated', animationName)
+
+  function handleAnimationEnd() {
+      node.classList.remove('animated', animationName)
+      node.removeEventListener('animationend', handleAnimationEnd)
+
+      if (typeof callback === 'function') callback()
+  }
+
+  node.addEventListener('animationend', handleAnimationEnd)
+}
